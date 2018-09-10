39d38e28479ff5d90dfd89f54195d8b05530c725
xiaotian.huang <hsiaotien@163.com>
2018-09-11 00:21:55 +0800

    Initial commit

---
+++ src/main/java/com/dev/guide03/Guide3Application.java
@@ -0,0 +1,12 @@
+package com.dev.guide03;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Guide3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Guide3Application.class,args);
+    }
+}
---
+++ src/main/java/com/dev/guide03/config/JdbcConfig.java
@@ -0,0 +1,21 @@
+package com.dev.guide03.config;
+
+import com.alibaba.druid.pool.DruidDataSource;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+/**
+ * 更为优雅的注入方式
+ */
+@Configuration
+public class JdbcConfig {
+    @Bean
+    // 声明要注入的属性前缀，SpringBoot会自动把相关属性通过set方法注入到DataSource中
+    @ConfigurationProperties(prefix = "jdbc")
+    public DataSource dataSource() {
+        DruidDataSource dataSource = new DruidDataSource();
+        return dataSource;
+    }
+}
